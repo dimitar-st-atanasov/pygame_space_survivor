784a6b90cdef5bb6b58c5f00da944f5651090fd0
dimitar-st-atanasov <dimitar.st.atanasov@gmail.com>
2024-12-26 04:57:52 +0100

    first version of the game

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# pygame_space_survivor
 A galaxy game where you have to avoid crashes with asteroids.
+
+1. To start the game:
+- install requirements
+- open new terminal
+- run main.py file
+  - for Windows: python main.py
+  - for (iOS or Linux): python3 main.py
+    
+2. Play instructions:
+
+Toolbar:
+  - Left top corner - Remaining lives
+  - Middle top - Time
+  - Right top corner - Destroyed missiles
+
+Receiving points:
+  - 1 points for each destroyed missile in the first 1 minute of the game;
+  - 5 points for each destroyed missile between 2nd and 4th minute of the game;
+  - 25 points for each destroyed missile after the start of the 5th minute of the game;
+
+Avoid hitting missiles with either moving the spaceship with left, right, up, down buttons or with shooting lasers with spacebar button. If 15 missiles destroyed in combo (without loosing live) - spaceship will start shooting 2 lasers at the same time.
+
+After every 60 seconds (1 minute) you receive a bonus live.
+
+You can pause/unpause the game with clicking on Esc button.
+
+End of the game is when you loose all your lives.
+
+Total points are calculated of all points from destroyed missiles + time seconds played.
+
+At the end you will be asked if you want to play again or close the system.
---
+++ main.py
@@ -0,0 +1,339 @@
+import pygame
+import time
+import random
+pygame.font.init()
+
+# Screen dimensions
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Survivor")
+
+# Load and scale background and toolbar
+BG = pygame.transform.scale(pygame.image.load("resources/images/space_background.jpg"), (WIDTH, HEIGHT - 60))
+TOOLBAR = pygame.transform.scale(pygame.image.load("resources/images/toolbar.png"), (WIDTH, HEIGHT - 740))
+
+LIVE_HEART_WIDTH = 30
+LIVE_HEART_HEIGHT = 35
+LIVE_HEART = pygame.transform.scale(pygame.image.load("resources/images/lives.png"), (LIVE_HEART_WIDTH, LIVE_HEART_HEIGHT))
+
+HIT_WIDTH = 35
+HIT_HEIGHT = 45
+HIT = pygame.transform.scale(pygame.image.load("resources/images/hit.png"), (HIT_WIDTH, HIT_HEIGHT))
+
+# Player dimensions
+SPACESHIP_WIDTH = 40
+SPACESHIP_HEIGHT = 50
+
+# Load and scale spaceship
+SPACESHIP = pygame.transform.scale(pygame.image.load("resources/images/spaceship.png"), (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+PLAYER_VEL = 5
+MISSILE_VEL = 5
+LASER_VEL = 10
+
+MISSILE_WIDTH = 20
+MISSILE_HEIGHT = 45
+MISSILE = pygame.transform.scale(pygame.image.load("resources/images/missile.png"), (MISSILE_WIDTH, MISSILE_HEIGHT))
+
+LASER_WIDTH = 2
+LASER_HEIGHT = 20
+
+# Font for rendering text
+FONT = pygame.font.SysFont("comicsans", 25)
+
+lost_text = FONT.render("Game over!", 1, "white")
+points_text = FONT.render("Total points: ", 1, "white")
+play_again_text = FONT.render("Play again?", 1, "white")
+yes_text = FONT.render("Yes -> Press Enter", 1, "white")
+no_text = FONT.render("No -> Press Esc", 1, "white")
+pause_text = FONT.render("Pause - Press ESC to resume", 1, "white")
+one_text = FONT.render("1", 1, "white")
+two_text = FONT.render("2", 1, "white")
+three_text = FONT.render("3", 1, "white")
+
+def draw(player, elapsed_time, missiles, lasers, live_hearts, hit, extra_points):
+
+    # Render and display background and toolbar
+    WIN.blit(TOOLBAR, (0, 0))
+    WIN.blit(BG, (0, 61))
+
+    # Render and display remaining lives text
+    show_hits = FONT.render(f"Remaining lives:", 1, "white")
+    WIN.blit(show_hits, (25, 8))
+
+    # Render and display destroyed missiles number
+    show_destroyed_missiles = FONT.render(f"Destroyed missiles: {len(extra_points)}", 1, "white")
+    WIN.blit(show_destroyed_missiles, (700, 8))
+
+    # Render and display live hearts
+    for i in range(live_hearts):
+        WIN.blit(LIVE_HEART, (210 + i * 40, 10))
+
+    # Render and display elapsed time
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (450, 8))
+
+    # Render and display the falling missiles
+    for missile in missiles:
+        WIN.blit(MISSILE, (missile.x, missile.y))
+        # pygame.draw.rect(WIN, (255, 0, 0), missile, 2) # for debugging
+
+    # Render and display the shooting laser
+    for laser in lasers:
+        pygame.draw.rect(WIN, "white", laser)
+
+    # Render and display hit
+    if hit:
+        WIN.blit(HIT, (player.x, player.y - 30))
+    
+    # Render and display spaceship at the player's position
+    WIN.blit(SPACESHIP, (player.x, player.y))
+    # pygame.draw.rect(WIN, (255, 0, 0), player, 2) # for debugging
+
+    pygame.display.update()
+
+
+def draw_last_hit(player, elapsed_time, live_hearts, extra_points):
+    '''Draw last hit when showing "Game over" text.'''
+
+    WIN.blit(TOOLBAR, (0, 0))
+    WIN.blit(BG, (0, 61))
+
+    show_hits = FONT.render(f"Remaining lives:", 1, "white")
+    WIN.blit(show_hits, (25, 8))
+
+    for i in range(live_hearts):
+        WIN.blit(LIVE_HEART, (210 + i * 40, 10))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (450, 8))
+
+    show_destroyed_missiles = FONT.render(f"Destroyed missiles: {len(extra_points)}", 1, "white")
+    WIN.blit(show_destroyed_missiles, (700, 8))
+
+    WIN.blit(HIT, (player.x, player.y - 20))
+    WIN.blit(SPACESHIP, (player.x, player.y))
+
+    pygame.display.update()
+
+def count_destroyed_missiles(last_minute, extra_points):
+    if last_minute <= 1:
+        extra_points.append("1")
+    elif last_minute >= 2 and last_minute <= 4:
+        extra_points.append("10")
+    elif last_minute >= 5:
+        extra_points.append("25")
+
+def main():
+
+    run = True
+
+    player = pygame.Rect(480, HEIGHT - SPACESHIP_HEIGHT - 5, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    clock = pygame.time.Clock()
+
+    # FIXME The time to show minutes and seconds. Not only seconds
+    start_time = time.time()
+    elapsed_time = 0
+
+    missile_add_increment = 2000 # Starting respawn time gap of falling missiles
+    missile_count = 0
+
+    lasers = []
+    missiles = []
+    live_hearts = 3
+    last_minute = 0
+    extra_points = []
+    total_extra_points = 0
+
+    last_shot_time = 0
+
+    current_hit_time = 0
+    hit = False
+    helper = False
+    combo_missile_destroyed = 0
+
+    while run:
+        missile_count += clock.tick(60)
+        elapsed_time = time.time() - start_time
+
+        # Earn extra lives
+        current_minute = int(elapsed_time // 60)  # Convert elapsed time to full minutes
+        if current_minute > last_minute:
+            if live_hearts < 5:
+                live_hearts += 1
+            last_minute = current_minute
+
+        if missile_count > missile_add_increment:
+            for _ in range(3):
+                missile_x = random.randint(0, WIDTH - MISSILE_WIDTH)
+                if missile_x < 10:
+                    missile_x = 10
+                elif missile_x > 975:
+                    missile_x = 975
+                missile = pygame.Rect(missile_x, 60, MISSILE_WIDTH, MISSILE_HEIGHT)
+                missiles.append(missile)
+
+            missile_add_increment = max(250, missile_add_increment - 30)
+            missile_count = 0
+
+        pause = False
+
+        # MOVE player - left, right, up and down
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL - 5 >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width + 5 <= WIDTH:
+            player.x += PLAYER_VEL
+        if keys[pygame.K_UP] and player.y - PLAYER_VEL - 65 >= 0:
+            player.y -= PLAYER_VEL
+        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height + 5 <= HEIGHT:
+            player.y += PLAYER_VEL
+        if keys[pygame.K_SPACE]:
+            current_time = time.time()
+            if len(extra_points) < 15 or helper == False:
+                if len(lasers) <= 0:
+                    laser = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2, player.y, LASER_WIDTH, LASER_HEIGHT)
+                    lasers.append(laser)
+            if len(extra_points) >= 15 and int(current_time) - int(last_shot_time) >= 0.4 and helper == True:
+                if len(lasers) <= 1:
+                    laser1 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 - 7, player.y, LASER_WIDTH, LASER_HEIGHT)
+                    lasers.append(laser1)
+                    laser2 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 + 7, player.y, LASER_WIDTH, LASER_HEIGHT)
+                    lasers.append(laser2)
+                    last_shot_time = current_time
+        if keys[pygame.K_ESCAPE]:
+            pause = True
+
+        # Pause/unpause the game with ESC button
+        if pause:
+            WIN.blit(pause_text, (WIDTH / 2 - pause_text.get_width() / 2, HEIGHT / 2 - pause_text.get_height() / 2))
+            pygame.display.update()
+            while pause:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+                        pause = False
+                        break
+
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                        pause = False
+                        for _, text in zip(range(3, 0, -1), [three_text, two_text, one_text]):
+                            WIN.blit(BG, (0, 61))
+                            WIN.blit(TOOLBAR, (0, 0))
+                            draw(player, elapsed_time, missiles, lasers, live_hearts, hit, extra_points)
+                            
+                            WIN.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2 + 50))
+                            pygame.display.update()
+                            pygame.time.delay(1000)
+                        break
+        
+        # Generate falling missiles and remove them if hit spaceship
+        for missile in missiles[:]:
+            missile.y += MISSILE_VEL
+            if missile.y > HEIGHT:
+                missiles.remove(missile)
+            elif missile.x + missile.width > WIDTH:
+                missile.x = WIDTH - missile.width
+            elif missile.y + missile.height >= player.y and missile.colliderect(player):
+                missiles.remove(missile)
+                hit = True
+                live_hearts -= 1
+                helper = False
+                combo_missile_destroyed = 0
+                current_hit_time = time.time()
+                count_destroyed_missiles(last_minute, extra_points)
+                break
+
+        if hit:
+            if time.time() - current_hit_time >= 0.5:  # remove hit image after half a second after a hit
+                hit = False
+        
+        # Generate shooting laser and remove laser and missile if hit
+        for laser in lasers[:]:
+            laser.y -= LASER_VEL
+            if laser.y <= 60:
+                try:
+                    lasers.remove(laser)
+                except ValueError:
+                    pass
+                
+            for missile in missiles[:]:
+                if laser.colliderect(missile):
+                    count_destroyed_missiles(last_minute, extra_points)
+                    try:
+                        missiles.remove(missile)
+                        combo_missile_destroyed += 1
+                        if combo_missile_destroyed >= 15:
+                            helper = True
+                    except ValueError:
+                        pass
+
+                    if len(lasers) > 0:
+                        try:
+                            lasers.remove(laser)
+                        except ValueError:
+                            pass
+                    break
+        
+        # Game Over
+        if live_hearts == 0:
+            draw_last_hit(player, elapsed_time, live_hearts, extra_points)
+            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(2500)
+
+            for point in extra_points:
+                total_extra_points += int(point)
+
+            for _ in range(int(elapsed_time)):
+                WIN.blit(TOOLBAR, (0, 0))
+                WIN.blit(BG, (0, 61))
+                total_extra_points += 1
+                # To center the text according the length of the string text + total_extra_points
+                total_points_text = FONT.render(f"Total points: {total_extra_points}", 1, "white")
+                WIN.blit(total_points_text, (WIDTH / 2 - len(str(total_extra_points)) * 10 - points_text.get_width() / 2, HEIGHT / 2 - points_text.get_height() / 2))
+                pygame.display.update()
+                pygame.time.delay(20)
+
+            # raise smoothly whole text with 100 pixels
+            up_rising = 0
+            for _ in range(100):
+                up_rising += 1
+                total_points_text = FONT.render(f"Total points: {total_extra_points}", 1, "white")
+                WIN.blit(TOOLBAR, (0, 0))
+                WIN.blit(BG, (0, 61))
+                WIN.blit(total_points_text, (WIDTH / 2 - len(str(total_extra_points)) * 10 - points_text.get_width() / 2, HEIGHT / 2 - points_text.get_height() / 2 - up_rising))
+                WIN.blit(play_again_text, (WIDTH/2 - play_again_text.get_width()/2, HEIGHT/2 - play_again_text.get_height()/2 + 80 - up_rising))
+                WIN.blit(yes_text, (WIDTH/2 - yes_text.get_width()/2, HEIGHT/2 - yes_text.get_height()/2 + 140 - up_rising))
+                WIN.blit(no_text, (WIDTH/2 - no_text.get_width()/2, HEIGHT/2 - no_text.get_height()/2 + 180 - up_rising))
+                pygame.display.update()
+            
+            given_answer = False
+            while not given_answer:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        given_answer = True
+                        run = False
+                        break
+
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RETURN:
+                            given_answer = True
+                            main()
+                        elif event.key == pygame.K_ESCAPE:
+                            given_answer = True
+                            pygame.quit()
+                            exit() 
+
+        # Exit game with "x" icon
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(player, elapsed_time, missiles, lasers, live_hearts, hit, extra_points)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
