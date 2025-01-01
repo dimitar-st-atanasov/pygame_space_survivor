b799370bc86c3ddbfc7edde076830cb6a9e357e6
dimitar-st-atanasov <dimitar.st.atanasov@gmail.com>
2025-01-01 19:35:09 +0100

    Add sounds and generate missiles at least 2 * MISSILE_WIDTH from each other

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import time
 import random
 pygame.font.init()
+pygame.mixer.init()
+from pygame.locals import *
 
 # Screen dimensions
 WIDTH, HEIGHT = 1000, 800
@@ -32,7 +34,7 @@ SHIELD_HUNDRED_POINTS = pygame.transform.scale(pygame.image.load("resources/imag
 
 PLAYER_VEL = 5
 MISSILE_VEL = 4
-LASER_VEL = 10
+LASER_VEL = 15
 EXTRA_LIVE_HEART_VEL = 3
 
 MISSILE_WIDTH = 20
@@ -50,10 +52,59 @@ points_text = FONT.render("Total points: ", 1, "white")
 play_again_text = FONT.render("Play again?", 1, "white")
 yes_text = FONT.render("Yes -> Press Enter", 1, "white")
 no_text = FONT.render("No -> Press Esc", 1, "white")
-pause_text = FONT.render("Pause -> Press 'P' or Esc to resume the game", 1, "white")
+pause_text = FONT.render("Pause -> Press 'P' to resume the game or Esc to quit", 1, "white")
 one_text = FONT.render("1", 1, "white")
 two_text = FONT.render("2", 1, "white")
 three_text = FONT.render("3", 1, "white")
+
+# Initialize mixer with enough channels
+pygame.mixer.set_num_channels(13)
+
+# Assign channels for different sounds
+SHOOT_SINGLE_CHANNEL = pygame.mixer.Channel(0)
+SHOOT_DOUBLE_CHANNEL = pygame.mixer.Channel(1)
+DESTROY_MISSILE_CHANNEL = pygame.mixer.Channel(2)
+GETTING_HIT_CHANNEL = pygame.mixer.Channel(3)
+COLLECT_HEART_CHANNEL = pygame.mixer.Channel(4)
+ACTIVATE_SHIELD_CHANNEL = pygame.mixer.Channel(5)
+DEACTIVATE_SHIELD_CHANNEL = pygame.mixer.Channel(6)
+COUNTDOWN_CHANNEL = pygame.mixer.Channel(7)
+COLLECT_POINTS_CHANNEL = pygame.mixer.Channel(8)
+GAME_OVER_CHANNEL = pygame.mixer.Channel(9)
+UPGRADE_CHANNEL = pygame.mixer.Channel(10)
+GENERATE_HEART_CHANNEL = pygame.mixer.Channel(10)
+MENU_SONG_CHANNEL = pygame.mixer.Channel(11)
+
+# Load Sounds
+shoot_single_sound = pygame.mixer.Sound("resources/sounds/shoot_single.mp3")
+shoot_double_sound = pygame.mixer.Sound("resources/sounds/shoot_double.mp3")
+destroy_missile_sound = pygame.mixer.Sound("resources/sounds/destroy_missile.wav")
+getting_hit_sound = pygame.mixer.Sound("resources/sounds/getting_hit.wav")
+collect_heart_sound = pygame.mixer.Sound("resources/sounds/collect_heart.mp3")
+activate_shield_sound = pygame.mixer.Sound("resources/sounds/activate_shield.wav")
+deactivate_shield_sound = pygame.mixer.Sound("resources/sounds/deactivate_shield.wav")
+countdown_sound = pygame.mixer.Sound("resources/sounds/countdown.wav")
+collect_points_sound = pygame.mixer.Sound("resources/sounds/collect_points.wav")
+game_over_sound = pygame.mixer.Sound("resources/sounds/game_over.wav")
+upgrade_sound = pygame.mixer.Sound("resources/sounds/upgrade.wav")
+generate_heart_sound = pygame.mixer.Sound("resources/sounds/generate_heart.wav")
+menu_song = pygame.mixer.Sound("resources/sounds/menu_song.wav")
+
+# Adjust Volume (Optional)
+shoot_single_sound.set_volume(0.2)
+shoot_double_sound.set_volume(0.2)
+destroy_missile_sound.set_volume(1.0)
+getting_hit_sound.set_volume(1.0)
+collect_heart_sound.set_volume(1.0)
+activate_shield_sound.set_volume(0.8)
+deactivate_shield_sound.set_volume(1.0)
+countdown_sound.set_volume(1.0)
+collect_points_sound.set_volume(0.7)
+game_over_sound.set_volume(1.0)
+upgrade_sound.set_volume(1.0)
+generate_heart_sound.set_volume(0.7)
+menu_song.set_volume(1.0)
+
 
 def draw(player, elapsed_time, missiles, lasers, live_hearts, hit, combo_missile_destroyed, shield_enabled, extra_live_hearts):
 
@@ -72,8 +123,8 @@ def draw(player, elapsed_time, missiles, lasers, live_hearts, hit, combo_missile
         show_destroyed_missiles_number_text = FONT.render(f"{combo_missile_destroyed}", 1, "green")   
     
     show_destroyed_missiles_text = FONT.render(f"Combo:", 1, "white")
-    WIN.blit(show_destroyed_missiles_text, (830, 10))
-    WIN.blit(show_destroyed_missiles_number_text, (920, 10))
+    WIN.blit(show_destroyed_missiles_text, (810, 15))
+    WIN.blit(show_destroyed_missiles_number_text, (900, 15))
 
     # Render and display live hearts
     for i in range(live_hearts):
@@ -117,23 +168,14 @@ def draw(player, elapsed_time, missiles, lasers, live_hearts, hit, combo_missile
     pygame.display.update()
 
 
-def draw_last_hit(player, elapsed_time, live_hearts, extra_points):
+def draw_last_hit(player, elapsed_time):
     '''Draw last hit when showing "Game over" text.'''
 
     WIN.blit(TOOLBAR, (0, 0))
     WIN.blit(BG, (0, 61))
 
-    show_hits = FONT.render(f"Remaining lives:", 1, "white")
-    WIN.blit(show_hits, (25, 8))
-
-    for i in range(live_hearts):
-        WIN.blit(LIVE_HEART, (210 + i * 40, 10))
-
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (450, 8))
-
-    show_destroyed_missiles = FONT.render(f"Destroyed missiles: {len(extra_points)}", 1, "white")
-    WIN.blit(show_destroyed_missiles, (700, 8))
 
     WIN.blit(HIT, (player.x, player.y - 20))
     WIN.blit(SPACESHIP, (player.x, player.y))
@@ -141,6 +183,7 @@ def draw_last_hit(player, elapsed_time, live_hearts, extra_points):
     pygame.display.update()
 
 def count_destroyed_missiles(last_minute, extra_points):
+
     if last_minute <= 1:
         extra_points.append("1")
     elif last_minute >= 2 and last_minute <= 4:
@@ -149,6 +192,7 @@ def count_destroyed_missiles(last_minute, extra_points):
         extra_points.append("25")
 
 def main():
+    
     global MISSILE_VEL
 
     run = True
@@ -164,6 +208,7 @@ def main():
 
     missile_add_increment = 2000 # Starting respawn time gap of falling missiles
     missile_count = 0
+    min_distance = MISSILE_WIDTH * 2
 
     lasers = []
     missiles = []
@@ -181,8 +226,9 @@ def main():
     shield_enabled = False
     extra_live_heart_enabled = False
     extra_live_hearts = []
+    upgrade_enabled = False
 
-    last_minute_increment = 2
+    last_minute_increment = 1
 
     while run:
         missile_count += clock.tick(60)
@@ -196,14 +242,22 @@ def main():
             last_minute = current_minute
 
         if missile_count > missile_add_increment:
+
             for _ in range(3):
-                missile_x = random.randint(0, WIDTH - MISSILE_WIDTH)
-                if missile_x < 10:
-                    missile_x = 10
-                elif missile_x > 975:
-                    missile_x = 975
-                missile = pygame.Rect(missile_x, 60, MISSILE_WIDTH, MISSILE_HEIGHT)
-                missiles.append(missile)
+                attempts = 0
+                while attempts < 100:
+                    missile_x = random.randint(0, WIDTH - MISSILE_WIDTH)
+                    # Extract x-coordinates from existing missiles for distance check
+                    existing_x_coords = [m.x for m in missiles]
+                    # Check if this missile is far enough from all existing missiles
+                    if all(abs(missile_x - existing_x) >= min_distance for existing_x in existing_x_coords):
+                        break
+                    attempts += 1
+                # If a valid position is found within the limit, create the missile
+                if attempts < 100:
+                    missile_x = max(10, min(missile_x, WIDTH - MISSILE_WIDTH - 10))
+                    missile = pygame.Rect(missile_x, 60, MISSILE_WIDTH, MISSILE_HEIGHT)
+                    missiles.append(missile)
 
             missile_add_increment = max(250, missile_add_increment - 30)
             missile_count = 0
@@ -222,24 +276,30 @@ def main():
             player.y += PLAYER_VEL
         if keys[pygame.K_SPACE]:
             current_time = time.time()
-            if helper == False:
-                if len(lasers) <= 0:
-                    laser = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2, player.y, LASER_WIDTH, LASER_HEIGHT)
-                    lasers.append(laser)
-            if helper == True and int(current_time) - int(last_shot_time) >= 0.3:
-                if len(lasers) <= 1:
-                    laser1 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 - 7, player.y, LASER_WIDTH, LASER_HEIGHT)
-                    lasers.append(laser1)
-                    laser2 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 + 7, player.y, LASER_WIDTH, LASER_HEIGHT)
-                    lasers.append(laser2)
-                    last_shot_time = current_time
+            if helper == False and ((len(lasers) <= 0 and current_time - last_shot_time >= 0.4) or current_time - last_shot_time >= 0.7):
+                laser = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2, player.y, LASER_WIDTH, LASER_HEIGHT)
+                SHOOT_SINGLE_CHANNEL.play(shoot_single_sound)
+                lasers.append(laser)
+                last_shot_time = current_time
+            if helper and ((len(lasers) <= 1 and current_time - last_shot_time >= 0.4) or current_time - last_shot_time >= 0.7):
+                laser1 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 - 7, player.y, LASER_WIDTH, LASER_HEIGHT)
+                lasers.append(laser1)
+                laser2 = pygame.Rect(player.x + SPACESHIP_WIDTH // 2 - LASER_WIDTH // 2 + 7, player.y, LASER_WIDTH, LASER_HEIGHT)
+                SHOOT_DOUBLE_CHANNEL.play(shoot_double_sound)
+                lasers.append(laser2)
+                last_shot_time = current_time
         if keys[pygame.K_ESCAPE] or keys[pygame.K_p]:
             pause = True
 
-        # Pause/unpause the game with "P" or ESC key
+        # Pause/unpause the game with "P" or ESC key to quit
         if pause:
             pause_start = time.time()
+            MENU_SONG_CHANNEL.play(menu_song, loops=-1)
             WIN.blit(pause_text, (WIDTH / 2 - pause_text.get_width() / 2, HEIGHT / 2 - pause_text.get_height() / 2))
+            
+            # Clear the event queue before waiting for user input
+            pygame.event.get()
+
             pygame.display.update()
             while pause:
                 for event in pygame.event.get():
@@ -248,9 +308,16 @@ def main():
                         pause = False
                         break
 
-                    if event.type == pygame.KEYDOWN and (event.key == pygame.K_p or event.key == pygame.K_ESCAPE):
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                        run = False
+                        pause = False
+                        break
+
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                        MENU_SONG_CHANNEL.stop()
                         pause = False
                         for _, text in zip(range(3, 0, -1), [three_text, two_text, one_text]):
+                            COUNTDOWN_CHANNEL.play(countdown_sound)
                             WIN.blit(BG, (0, 61))
                             WIN.blit(TOOLBAR, (0, 0))
                             draw(player, elapsed_time, missiles, lasers, live_hearts, hit, combo_missile_destroyed, shield_enabled, extra_live_hearts)
@@ -261,7 +328,7 @@ def main():
                         paused_time += time.time() - pause_start
                         break
         
-        # Generate falling missiles and remove them if hit spaceship
+        # Generate falling missiles and remove them if crash into spaceship
         for missile in missiles[:]:
             if current_minute > last_minute_increment:
                 MISSILE_VEL += 1
@@ -274,7 +341,9 @@ def main():
             elif missile.y + missile.height >= player.y and missile.colliderect(player):
                 if shield_enabled:
                     shield_enabled = False
+                    DEACTIVATE_SHIELD_CHANNEL.play(deactivate_shield_sound)
                 else:
+                    GETTING_HIT_CHANNEL.play(getting_hit_sound)
                     live_hearts -= 1
                     helper = False
                     hit = True
@@ -302,13 +371,21 @@ def main():
                     count_destroyed_missiles(last_minute, extra_points)
                     try:
                         missiles.remove(missile)
+                        DESTROY_MISSILE_CHANNEL.play(destroy_missile_sound)
                         combo_missile_destroyed += 1
                         if combo_missile_destroyed >= 15:
                             helper = True
+                            upgrade_enabled = True
+                        if combo_missile_destroyed == 15 and upgrade_enabled:
+                            upgrade_enabled = False
+                            UPGRADE_CHANNEL.play(upgrade_sound)
                         if combo_missile_destroyed >= 30:
-                            shield_enabled = True
+                            if not shield_enabled:
+                                shield_enabled = True
+                                ACTIVATE_SHIELD_CHANNEL.play(activate_shield_sound)
                         if combo_missile_destroyed % 50 == 0 and combo_missile_destroyed > 0:
                             extra_live_heart_enabled = True
+                            GENERATE_HEART_CHANNEL.play(generate_heart_sound)
 
                     except ValueError:
                         pass
@@ -333,6 +410,7 @@ def main():
                 extra_live_heart.x = WIDTH - extra_live_heart.width
             if player.colliderect(extra_live_heart):
                 extra_live_hearts.remove(extra_live_heart)
+                COLLECT_HEART_CHANNEL.play(collect_heart_sound)
                 if live_hearts < 5:
                     live_hearts += 1
                 extra_points.append("100")
@@ -340,10 +418,12 @@ def main():
 
         # Game Over
         if live_hearts == 0:
-            draw_last_hit(player, elapsed_time, live_hearts, extra_points)
+            pygame.time.delay(100)
+            draw_last_hit(player, elapsed_time)
             WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
             pygame.display.update()
-            pygame.time.delay(2500)
+            GAME_OVER_CHANNEL.play(game_over_sound)
+            pygame.time.delay(5000)
 
             for point in extra_points:
                 total_extra_points += int(point)
@@ -355,6 +435,7 @@ def main():
                 # To center the text according the length of the string text + total_extra_points
                 total_points_text = FONT.render(f"Total points: {total_extra_points}", 1, "white")
                 WIN.blit(total_points_text, (WIDTH / 2 - len(str(total_extra_points)) * 10 - points_text.get_width() / 2, HEIGHT / 2 - points_text.get_height() / 2))
+                COLLECT_POINTS_CHANNEL.play(collect_points_sound)
                 pygame.display.update()
                 pygame.time.delay(10)
             
@@ -374,6 +455,7 @@ def main():
                 WIN.blit(no_text, (WIDTH/2 - no_text.get_width()/2, HEIGHT/2 - no_text.get_height()/2 + 180 - up_rising))
                 pygame.display.update()
             
+            MENU_SONG_CHANNEL.play(menu_song, loops=-1)
             given_answer = False
             while not given_answer:
                 for event in pygame.event.get():
@@ -385,6 +467,7 @@ def main():
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_RETURN:
                             given_answer = True
+                            MENU_SONG_CHANNEL.stop()
                             main()
                         elif event.key == pygame.K_ESCAPE:
                             given_answer = True
